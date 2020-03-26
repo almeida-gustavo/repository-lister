190eb66c3ddc26d3e2dfc95b685235458cd460aa
melo_gustavo <gustavomelo95@gmail.com>
2020-03-26 15:45:05 -0300

    Main and Repository pages

---
+++ src/pages/Main/index.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    error: null,
+  };
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({
+        repositories: JSON.parse(repositories),
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true, error: false });
+    try {
+      const { newRepo, repositories } = this.state;
+
+      // eslint-disable-next-line no-throw-literal
+      if (newRepo === '')
+        throw new Error('Voce precisa indicar um repositorio');
+
+      const hasRepo = repositories.find((r) => r.name === newRepo.name);
+
+      // eslint-disable-next-line no-throw-literal
+      if (hasRepo) throw new Error('Repositorio Duplicado');
+
+      const response = await api.get(`/repos/${newRepo}`);
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+      });
+    } catch (err) {
+      this.setState({ error: true });
+      // eslint-disable-next-line no-alert
+      alert(err);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { newRepo, loading, repositories, error } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositorios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit} error={error}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositorio"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map((repository) => (
+            <li key={repository.name}>
+              <span>{repository.name} </span>
+              {/* esse encode serve para vc nao deixar ir o / no url e ser como se fosse uma pasta a mais, porque o nome que agnt tem do repositorio eh ex: melogustavo/buefy */}
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
